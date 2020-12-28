7a9ded0abd73f1068fa927fc8240127f8a661c46
wanting <kashjack@163.com>
2020-12-28 18:26:33 +0800

    v0.1.1

---
+++ JKBase/Classes/JKDate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  JKBase
+//
+//  Created by kashjack on 12/25/2020.
+//  Copyright (c) 2020 kashjack. All rights reserved.
+//
+
+
+import UIKit
+
+public extension Date {
+
+
+}
